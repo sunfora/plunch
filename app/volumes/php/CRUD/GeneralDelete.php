7f2381114ca00bd2b5f8e44e6d8e160bd8c01754
Ivan Sandovin <sandovin@niuitmo.ru>
2023-02-24 09:27:43 +0300

    traits dropped

--- app/volumes/php/CRUD/GeneralDelete.php
+++
@@ -1,8 +0,0 @@
-<?
-namespace Plunch\CRUD;
-
-trait GeneralDelete {
-    private function general_delete($entity) {
-        return $this->db->delete(self::TABLE, '%l', $this->locate($entity));
-    } 
-}
--- app/volumes/php/CRUD/GeneralReplace.php
+++
@@ -1,8 +0,0 @@
-<?
-namespace Plunch\CRUD;
-
-trait GeneralReplace {
-    private function general_replace($entity) {
-        return $this->db->replace(self::TABLE, $this->row_from_entity($entity));
-    } 
-}
